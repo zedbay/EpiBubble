fc2bf546aa9341be6c6d8050cffa3fde1014e5e7
Antoine HEURTAULT <antoine.heurtault@epitech.eu>
2019-01-09 11:25:31 +0100

    .

---
+++ Assets/Scripts/MooveMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MooveMenu : MonoBehaviour
+{
+
+    public Button m_resume;
+    public Button m_settings;
+    public Button m_return_left;
+    public Button m_return_right;
+
+    public GameObject m_menu;
+    public GameObject m_menu_settings;
+    public GameObject m_menu_resume;
+
+
+    private bool moove_left = false;
+    private bool moove_right = false;
+    private bool return_right = false;
+    private bool return_left = false;
+
+    private 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_resume.onClick.AddListener(ShowResume);
+        m_settings.onClick.AddListener(ShowSettings);
+        m_return_left.onClick.AddListener(ShowMenu);
+        m_return_right.onClick.AddListener(ShowMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ShowMenu()
+    {
+        m_menu_settings.SetActive(false);
+        m_menu.SetActive(true);
+        m_menu_resume.SetActive(false);
+    }
+
+    private void ShowSettings()
+    {
+        m_menu_settings.SetActive(true);
+        m_menu.SetActive(false);
+    }
+
+    private void ShowResume()
+    {
+        m_menu.SetActive(false);
+        m_menu_resume.SetActive(true);
+    }
+
+
+}
---
+++ Assets/Scripts/RaccourciVolume.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaccourciVolume : MonoBehaviour
+{
+
+    public Button m_volume;
+
+    public Sprite m_soundOn;
+    public Sprite m_soundOff;
+
+    private bool m_mooveMenuPrincipal = false;
+    private float smoothTime = 0.3F;
+    private float yVelocity = 0.0F;
+
+
+
+    void Start()
+    {
+        m_volume.onClick.AddListener(OnClickVolume);
+        m_volume.image.sprite = m_soundOn;
+    }
+
+    void OnClickVolume()
+    {
+        if(m_volume.image.sprite == m_soundOn)
+        {
+            m_volume.image.sprite = m_soundOff;
+        }
+        else
+        {
+            m_volume.image.sprite = m_soundOn;
+        }
+
+    }
+
+    void OnClickParametres()
+    {
+        m_mooveMenuPrincipal = true;
+    }
+
+    void OnClickQuitter()
+    {
+        Debug.Log("Au revoid");
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (m_mooveMenuPrincipal)
+        {
+            //m_menuPrincpal.transform.Translate(new Vector2(0, 16));
+            //if (m_menuPrincpal.transform.position.y >= 900)
+               // m_mooveMenuPrincipal = false; 
+        }
+    }
+}
