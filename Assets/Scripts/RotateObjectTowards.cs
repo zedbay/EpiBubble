925bb140d3074813f7990239b42a2c650e21ceba
Antoine HEURTAULT <antoine.heurtault@epitech.eu>
2019-01-14 15:35:11 +0100

    merge projet farouk et antoine

---
+++ Assets/Scripts/RotateObjectTowards.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RotateObjectTowards : MonoBehaviour
+{
+    public float speed = 10;
+
+    private void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
